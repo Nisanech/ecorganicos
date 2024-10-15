a0d58e53bd2a27930e8e97bb75d9d9a5eacd4f82
rrinconinnerconsulting <rrincon@innerconsulting.com>
2024-10-15 18:28:09 -0500

    creación rama home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import CardHome from "@/presentation/home/components/CardHome";
 
+
 export default function Home() {
   return (
     <div className="grid items-center justify-items-center gap-2">
+
+      <h1>hola</h1>
 
     </div>
   );
